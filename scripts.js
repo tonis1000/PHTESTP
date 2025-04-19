e11b7983b7422fd0438670d6048fb9741af1c25f
tonis1000 <atonis@live.com>
2025-04-20 01:31:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1105,7 +1105,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-let lastSentCache = {};
 
 function hasStreamCacheChanged() {
   return JSON.stringify(globalStreamCache) !== JSON.stringify(lastSentCache);
