8b70aeb8f5171554e4fdb495b1ed81427a9e4967
tonis1000 <atonis@live.com>
2025-04-24 19:10:36 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -653,9 +653,17 @@ function isDirectStream(url) {
   return /\.(m3u8|ts|mp4|mpd|webm)$/i.test(url);
 }
 
-function isSTRM(url) {
-  return url.endsWith('.strm');
+function isIframeStream(url) {
+  url = url.toLowerCase();
+  return (
+    /embed|\.php$|\.html$/.test(url) ||
+    url.includes('anacon.org') ||
+    url.includes('lakatamia.tv') ||
+    url.includes('sportskeeda') ||
+    url.includes('worldstreams.tv')
+  );
 }
+
 
 async function resolveSTRM(url) {
   try {
