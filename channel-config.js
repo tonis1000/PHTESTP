0f0d055aba8321b94a8ec51b2c336ebfdae13466
tonis1000 <atonis@live.com>
2025-05-08 19:35:41 +0200

    Update and rename playlist-loader.js to channel-config.js

---
+++ channel-config.js
@@ -0,0 +1,11 @@
+window.favoriteChannels = new Map([
+  ["ERT1",  { logo: "logos/ert1.png",  epgId: "ert1" }],
+  ["MEGA",  { logo: "logos/mega.png",  epgId: "mega" }],
+  ["STAR",  { logo: "logos/star.png",  epgId: "star" }],
+  ["ANT1",  { logo: "logos/ant1.png",  epgId: "ant1" }],
+  ["ALPHA", { logo: "logos/alpha.png", epgId: "alpha" }],
+  ["SKAI",  { logo: "logos/skai.png",  epgId: "skai" }],
+  ["OPEN",  { logo: "logos/open.png",  epgId: "open" }]
+]);
+
+window.availableStreams = {};
